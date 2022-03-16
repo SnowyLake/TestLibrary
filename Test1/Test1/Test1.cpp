0c8db9c52eb17b4a011cd18f5c54f735a6647ab7
SnowyLake <snowylake.dev@outlook.com>
2022-03-17 03:08:38 +0800

    test

--- Test1/Test1/Test1.cpp
+++ Test1/Test1/Test1.cpp
@@ -1,16 +1,41 @@
-﻿#include <__msvc_all_public_headers.hpp>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <__msvc_all_public_headers.hpp>
+#include <Windows.h>
 using namespace std;
-#define Range std::ranges
+#define stdr std::ranges
+#define stdrv std::ranges::views
+
+namespace Swy
+{
+template<typename T>
+concept IsNotString = !requires{ std::same_as<T, std::string> && std::same_as<T, std::string_view>; };
 
 template<typename... Args>
-inline void Print(const std::string_view fmt, const Args&... args)
+inline void Print(std::string_view fmt, const Args&... args)
 {
 	std::cout << std::vformat(fmt, std::make_format_args(args...));
 }
+template<IsNotString T>
+inline void Print(T&& arg)
+{
+	std::cout << arg;
+}
+
 template<typename... Args>
-inline void Println(const std::string_view fmt, const Args&... args)
+inline void Println(std::string_view fmt, const Args&... args)
 {
 	std::cout << std::vformat(fmt, std::make_format_args(args...)) << std::endl;
+}
+template<IsNotString T>
+inline void Println(T&& arg)
+{
+	std::cout << arg << std::endl;
+}
+
+inline void Println()
+{
+	std::cout << std::endl;
+}
 }
 
 class FiveRulers
@@ -19,35 +44,35 @@ public:
 	using Val_t = std::string;
 public:
 	Val_t str = "Fuxk Cpp.\n";
-	FiveRulers() 
+	FiveRulers()
 	{ std::cout << "-------*ctor*-------\n"; }
 	~FiveRulers()
 	{ std::cout << "-------*dtor*-------\n"; }
 	FiveRulers(const FiveRulers&)
 	{ std::cout << "-------*copy ctor*-------\n"; }
-	FiveRulers(FiveRulers&&) noexcept
-	{ std::cout << "-------*move ctor*-------\n"; }
 	FiveRulers& operator=(const FiveRulers&)
 	{ std::cout << "-------*copy op= *-------\n"; return *this; }
+	FiveRulers(FiveRulers&&) noexcept
+	{ std::cout << "-------*move ctor*-------\n"; }
 	FiveRulers& operator=(FiveRulers&&) noexcept
 	{ std::cout << "-------*move op= *-------\n"; return *this; }
 
-	Val_t Func() { return str; }
+	Val_t Func() const { return str; }
 };
+
 struct MemoryLayout
 {
-	int32_t a;
-	short c;
-	char d;
-	int64_t b;
+
 };
 struct Null {};
 
+struct B {};
+struct D : B {};
+
+template<typename Container>
+concept IsVector = std::same_as<Container, std::vector<typename(Container::value_type)>>;
+
 int main()
 {
-	std::vector<int> v{5, 3, 4, 1, 2};
-	Range::sort(v, std::greater());
-	Range::for_each(v, [](const auto& x) {std::cout << std::format("{} ", x); });
-	std::stack<int> x;
-	x.pop();
+	
 }
